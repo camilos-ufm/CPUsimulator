c14e12812c761d54b18191488a2dd1a0b3fa14ab
danisnowman <danisnowman@gmail.com>
2019-08-24 13:21:37 -0600

    overloaded more f()s un PC

--- Simulator-Backend/PC.py
+++ Simulator-Backend/PC.py
@@ -2,14 +2,14 @@ from Register import Register
 
 
 class PC(Register):
-    data = [None] * 16
+    instructions = [None] * 16
 
     def __init__(self, manufacturer, build_date, purpose, storage, data):
         super().__init__(manufacturer, build_date, purpose, storage)
-        self.data = data
+        self.instructions.append(data)
     
     def setData(self, data):
-        self.data.append(data)
+        self.instructions.append(data)
     
     def getData(self):
-        return self.data
+        return self.instructions
