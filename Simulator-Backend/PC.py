231e2365a875f4dbe4cd37cfe16fe0168a9a7f56
danisnowman <danisnowman@gmail.com>
2019-08-24 14:34:58 -0600

    undid PC changes

--- Simulator-Backend/PC.py
+++ Simulator-Backend/PC.py
@@ -2,14 +2,14 @@ from Register import Register
 
 
 class PC(Register):
-    instructions = [None] * 16
+    instructions = 0
 
     def __init__(self, manufacturer, build_date, purpose, storage, data):
         super().__init__(manufacturer, build_date, purpose, storage)
-        self.instructions.append(data)
+        self.data = data
     
     def setData(self, data):
-        self.instructions.append(data)
+        self.data = data
     
     def getData(self):
-        return self.instructions
+        return self.data
