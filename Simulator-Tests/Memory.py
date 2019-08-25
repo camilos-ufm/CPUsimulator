15011cc1e71fcd84a1517bb93d6d3d951d8d3131
andresryes <enriquebolanos@ufm.edu>
2019-08-25 09:09:12 -0600

    added test_main.py to run unit tests

--- Simulator-Tests/Memory.py
+++
@@ -1,6 +0,0 @@
-from IC import *
-
-class Memory(IC):
-    def __init__(self, manufacturer, build_date, purpose, storage):
-        super().__init__(manufacturer, build_date, purpose)
-        self.storage = storage
