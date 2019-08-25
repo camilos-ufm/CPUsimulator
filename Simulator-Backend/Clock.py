32107d8fb3cb5658f311eb05ff25141c1de26703
andresryes <enriquebolanos@ufm.edu>
2019-08-24 23:23:42 -0600

    added Clock.py file

---
+++ Simulator-Backend/Clock.py
@@ -0,0 +1,6 @@
+from IC import *
+
+class Clock(IC):
+    def __init__(self, manufacturer, build_date, purpose, speed):
+        super().__init__(manufacturer, build_date, purpose)
+        self.storage = storage
